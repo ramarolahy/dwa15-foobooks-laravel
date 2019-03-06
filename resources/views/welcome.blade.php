c9a9feb0781a6c07284ef99c7fcda9aa6b068e88
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-03-06 14:01:21 -0700

    fix .gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <b>Laravel</b><br>
+                    Foobooks by Mbinintsoa
                 </div>
 
                 <div class="links">
